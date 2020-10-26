f8cffae0833924fea0b56f540a081a4b215b9d39
Ola Ouso <ouso.ola@gmail.com>
2020-10-26 20:46:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+   static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    let regex = /[^\w\s-']/g;
+    return string.replace(regex, "");
+  }
+  static titleize(string) {
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let newArr = [];
+    return newArr = string.split(" ").map((el, index) => {
+      while (index === 0) {
+        return el.charAt(0).toUpperCase() + el.slice(1)
+      }
+      if (!except.includes(el)) {
+        return el.charAt(0).toUpperCase() + el.slice(1)
+      } else {
+        return el
+      }
+    }).join(" ")
+
+  }
 }
